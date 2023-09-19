28b5b09b6ace3e2e8d33a65bf3e74a6ea2ebdd87
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2023-09-19 00:01:26 -0600

    Cambios 1er Entrega

--- validacion.js
+++ validacion.js
@@ -1,24 +1,37 @@
-const email = document.getElementById("emailLog")
-const pass = document.getElementById("passLog")
-const form = document.getElementById("formLogin")
-const parrafo = document.getElementById("warnings")
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("formLogin");
+  
+    form.addEventListener("submit", function (event) {
+      resetValidationMessages();
+      const emailInput = document.getElementById("emailLog");
+      const passInput = document.getElementById("passLog");
+  
+      let isValid = true;
+  
+      if (!isValidEmail(emailInput.value)) {
+        showValidationMessage("warningsEmail", "Ingrese un correo electrónico válido.");
+        isValid = false;
+      }
 
-form.addEventListener("submit", e=> {
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    parrafo.innerHTML = ""
-    if(regexEmail.test(email.value)){
-        warnings += 'El email no es valido <br>'
-        entrar = true
+  
+      if (passInput.value.length < 8) {
+        showValidationMessage("warningsPass", "La contraseña es incorrecta.");
+        isValid = false;
+      }
+ 
+    });
+  
+    function isValidEmail(email) {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
-    if(pass.value.length < 8){
-        warnings += 'La contraseña no es valida <br>'
-        entrar = true
+  
+    function resetValidationMessages() {
+      const warningElements = document.querySelectorAll(".warnings");
+      warningElements.forEach((element) => (element.textContent = ""));
     }
-
-    if (entrar){
-        parrafo.innerHTML = warnings
+  
+    function showValidationMessage(id, message) {
+      const warningElement = document.getElementById(id);
+      warningElement.textContent = message;
     }
-})
+  });
