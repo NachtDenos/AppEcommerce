a8e59debfcdd4a65a878f784d639af453d13341c
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2023-10-15 22:53:31 -0600

    Arreglos Login
    Arreglos del login para guardarlo en un session

--- Conexion/LoginUsuario.php
+++ Conexion/LoginUsuario.php
@@ -7,15 +7,17 @@
 		function login(){
 
 			 $kevin = $this->connectDB();
-
+			 
 			//Pasar el login, a esta parte
 				if (isset($_SESSION['usuario'])) {
 				header("Location: index.html");
 				exit();
 				}
-
 				session_start();
- 
+				
+				//Iniciamos la sesion que podremos usar de manera global en el programa
+
+				$_SESSION['Usuario'] = 'Kevin';
 
 				if (isset($_POST['botonLog']))
 				{
@@ -23,35 +25,62 @@
 				$User = $_POST['emailLog'];
 				$contra = $_POST['passLog'];
 				
-				$query = "SELECT Id_Usuario,Correo, Contraseña FROM usuarios WHERE Correo = :user AND Contraseña = :pass";
+				$query = "SELECT Id_Usuario,Correo, Contraseña,
+				ImagenPerfil, Nombre,
+				Sexo, Visibilidad, 
+				Rol  FROM usuarios WHERE Correo = :user AND Contraseña = :contra";
 				$kevin = $kevin->prepare($query);
 				$kevin->bindParam(':user', $User, PDO::PARAM_STR);
-				$kevin->bindParam(':pass', $contra, PDO::PARAM_STR);
-				//$kevin->execute();
+				$kevin->bindParam(':contra', $contra, PDO::PARAM_STR);
+				$kevin->execute();
 				echo ($query);
 
-				$result = $kevin->execute();
+				
+				
+
+				$result = $kevin->fetch(PDO::FETCH_ASSOC);
 				echo json_encode($result);
 	
 				if ($result == true) {
-					if ($kevin->rowCount() == 1) {
+					//if ($kevin->rowCount() == 1) {
 						// code...
-						$_SESSION['usuario'] = $User;
-						$kevin->closeConnection();
+
+						//$_FetchData = $result->fetch(PDO::FETCH_ASSOC);
+
+						$obj = array(
+							"id" => $_FetchData['Id_Usuario'],
+							"Mail" => $_FetchData['Correo'],
+							"Pass" => $_FetchData['Contraseña'],
+							"Img" => $_FetchData['ImagenPerfil'],
+							"Nombre" => $_FetchData['Nombre'],
+							"Sexo" => $_FetchData['Sexo'],
+							"Visibilidad" => $_FetchData['Visibilidad'],
+							"RolUsuario" => $_FetchData['Rol']
+						);
+						
+
+						$_SESSION['usuario'] = $obj;
+						
+						
+
 						header("Location: ../index.html");
 						exit();
-					}
-					else
-					{
-						$error = "Credenciales Incorrectas";
-						header("Location: ../login.html");
-						$kevin->closeConnection();
-					exit();
+				//	}
+					//else
+					//{
 					
-					}
+					//}
 					
 				
 				}else{
+					$error = "Credenciales Incorrectas";
+					echo $contra;
+					echo $User;
+					header("Location: ../login.html");
+					print_r ($kevin);
+					//header("Location: ../login.html");
+					
+				exit();
 					
 				}
 
