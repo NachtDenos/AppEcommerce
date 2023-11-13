20defc92797b47f6c6dd165edef9769b3c5b7725
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2023-11-13 00:37:16 -0600

    Update Register SP

--- ScriptsSQL/Querys.sql
+++ ScriptsSQL/Querys.sql
@@ -5,3 +5,12 @@ INSERT INTO usuarios (Correo, Usuario, Contraseña)
 VALUES ("Isaacpro553@gmail.com", "Wonder", "1234567");
 INSERT INTO usuarios (Correo, Usuario, Contraseña)
 VALUES ("Kevin@gmail.com", "Chevin", "Familia73");
+
+-- Query del SP de registro Usuarios
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistroUsuarios`(IN `CorreoP` VARCHAR(100) CHARSET utf8mb4, IN `UsuarioP` VARCHAR(100) CHARSET utf8mb4, IN `ContrasenaP` VARCHAR(100) CHARSET utf8mb4, IN `ImagenUsuario` BLOB, IN `NombreP` VARCHAR(100) CHARSET utf8mb4, IN `SexoP` VARCHAR(10) CHARSET utf8mb4, IN `VisibilidadP` INT(1) UNSIGNED, IN `RolP` INT(1) UNSIGNED)
+BEGIN
+INSERT INTO usuarios (usuarios.Correo, usuarios.Usuario, usuarios.Contraseña, usuarios.ImagenPerfil, usuarios.Nombre, usuarios.Sexo, usuarios.Visibilidad, usuarios.Rol)
+VALUES (CorreoP, UsuarioP, ContraseñaP, ImagenUsuario, NombreP, SexoP, VisibilidadP, RolP);
+END$$
+DELIMITER ;
