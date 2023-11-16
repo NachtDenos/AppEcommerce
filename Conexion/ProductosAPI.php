70697af91a0bc9e7d1f64d57e8fdaff024232dc2
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2023-11-16 10:24:49 -0600

    Primer Intento Insert Productos

--- Conexion/ProductosAPI.php
+++ Conexion/ProductosAPI.php
@@ -16,34 +16,55 @@ include_once 'ConexionPDP.php';
 
 class ProductosAPI extends DB
 {
-    function CrearProductos($NombreProd, $DescProd, $CantExistencia, $VideoProducto, $PrecioProd, $Visibilidad, $Foto1Prod, $Foto2Prod, $Foto3Prod, $Categoria, $Correo)
+    function CrearProductos($NombreProd, $DescProd, $CantExistencia, $VideoProducto, $PrecioProd, $Visibilidad, $Foto1Prod, $Foto2Prod, $Foto3Prod, $Categoria, $Correo, $TipoProducto)
     {
         $conn = $this->connectDB();
 
-        /* Tiene que ver para recibir el video
-        if (isset($_FILES["imagen"])) {
-			$imagen = file_get_contents($_FILES["imagen"]["tmp_name"]);
-			$imagenBlob = base64_encode($imagen);
+        // Tiene que ver para recibir el video
+        if (isset($_FILES["videoProd"])) {
+			$vid = file_get_contents($_FILES["videoProd"]["tmp_name"]);
+			$vidBlob = base64_encode($vid);
 			// Resto del código para procesar la imagen.
 		} else {
-			//echo $imagen;
+			echo $vid;
 			echo "El campo de imagen no se envió correctamente.";
 		}
-        */
+        
 
-        /*Fotos, copiar y pegar, cambiar valores
-        if (isset($_FILES["imagen"])) {
-			$imagen = file_get_contents($_FILES["imagen"]["tmp_name"]);
+        //Fotos, copiar y pegar, cambiar valores
+        if (isset($_FILES["imagenProd1"])) {
+			$imagen = file_get_contents($_FILES["imagenProd1"]["tmp_name"]);
 			$imagenBlob = base64_encode($imagen);
 			// Resto del código para procesar la imagen.
 		} else {
-			//echo $imagen;
+			echo $imagen;
 			echo "El campo de imagen no se envió correctamente.";
 		}
-        */
+
+        //Fotos, copiar y pegar, cambiar valores
+        if (isset($_FILES["imagenProd2"])) {
+			$imagen2 = file_get_contents($_FILES["imagenProd2"]["tmp_name"]);
+			$imagenBlob2 = base64_encode($imagen2);
+			// Resto del código para procesar la imagen.
+		} else {
+			echo $imagen2;
+			echo "El campo de imagen no se envió correctamente.";
+		}
+
+        //Fotos, copiar y pegar, cambiar valores
+        if (isset($_FILES["imagenProd3"])) {
+			$imagen3 = file_get_contents($_FILES["imagenProd3"]["tmp_name"]);
+			$imagenBlob3 = base64_encode($imagen3);
+			// Resto del código para procesar la imagen.
+		} else {
+			echo $imagen3;
+			echo "El campo de imagen no se envió correctamente.";
+		}
+        
+        
 
         //Los params deben llamarse igual que en el SP, al menos me ha tocado asi si no truena por no encontrar
-        $sql = "Call CrearProductos(:NombreProducto, :DescripcionProducto, :CantidadProducto, :VideoProducto, :PrecioProducto, :Visibilidad, :FotoProducto, :FotoProducto2, :FotoProducto3, :CorreoVendedor, :CategoriaNombre);";
+        $sql = "Call CrearProductos(:NombreProducto, :DescripcionProducto, :CantidadProducto, :VideoProducto, :PrecioProducto, :Visibilidad, :FotoProducto, :FotoProducto2, :FotoProducto3, :CorreoVendedor, :CategoriaNombre, :TipoProd);";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':NombreProducto', $NombreProd, PDO::PARAM_STR);
         $stmt->bindParam(':DescripcionProducto', $DescProd, PDO::PARAM_STR);
@@ -56,6 +77,7 @@ class ProductosAPI extends DB
         $stmt->bindParam(':FotoProducto3', $Foto3Prod, PDO::PARAM_STR);
         $stmt->bindParam(':CorreoVendedor', $Categoria, PDO::PARAM_STR);
         $stmt->bindParam(':CategoriaNombre', $Correo, PDO::PARAM_STR);
+        $stmt->bindParam(':TipoProd', $TipoProducto, PDO::PARAM_STR);
 
 
     }
